1cc23897974de3e56eed58804b0d7698da83e467
ptrvcs <petrovicsi.90@gmail.com>
2025-09-07 19:26:07 +0000

    implement time measurement

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <chrono>
 
 int calcualte_fibonacci_with_loop()
 {
@@ -26,9 +27,24 @@ void calcualte_fibonacci_with_recursion(int last_before_number, int last_number,
     calcualte_fibonacci_with_recursion(last_before_number, last_number, remaining - 1);
 }
 
+
+
 int main()
 {
+    using namespace std::chrono;
+
+    auto start_loop = high_resolution_clock::now();
     calcualte_fibonacci_with_loop();
+    auto end_loop = high_resolution_clock::now();
+    auto passed_loop = duration_cast<nanoseconds>(end_loop - start_loop).count();
+    std::cout << "Loop time: " << passed_loop << " ns" << std::endl;
+    
     std::cout << "-----" << std::endl;
+
+    auto start_rec = high_resolution_clock::now();
     calcualte_fibonacci_with_recursion(0, 1, 17);
+    auto end_rec = high_resolution_clock::now();
+    auto passed_rec = duration_cast<nanoseconds>(end_loop - start_loop).count();
+    std::cout << "Loop time: " << passed_rec << " ns" << std::endl;
+
 }
