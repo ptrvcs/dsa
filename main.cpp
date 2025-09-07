190efbe668f42660164bd4aa61b3a661d8a9c680
ptrvcs <petrovicsi.90@gmail.com>
2025-09-07 18:48:59 +0000

    Implement recursive version

--- main.cpp
+++ main.cpp
@@ -1,15 +1,34 @@
 #include <iostream>
 
-int main() {
+int calcualte_fibonacci_with_loop()
+{
     int last_before_number = 0;
     int last_number = 1;
 
-    for(int i = 0; i < 17; i++) {
+    for (int i = 0; i < 17; i++)
+    {
         int result = last_before_number + last_number;
         std::cout << result << std::endl;
         last_before_number = last_number;
-        last_number = result;       
-
+        last_number = result;
     }
-    
+}
+
+void calcualte_fibonacci_with_recursion(int last_before_number, int last_number, int remaining)
+{
+    if (remaining == 0)
+        return;
+
+    int result = last_before_number + last_number;
+    std::cout << result << std::endl;
+    last_before_number = last_number;
+    last_number = result;
+    calcualte_fibonacci_with_recursion(last_before_number, last_number, remaining - 1);
+}
+
+int main()
+{
+    calcualte_fibonacci_with_loop();
+    std::cout << "-----" << std::endl;
+    calcualte_fibonacci_with_recursion(0, 1, 17);
 }
