5d2afee95169a45547b4a32fc212376ad3bd97c2
ptrvcs <petrovicsi.90@gmail.com>
2025-09-09 18:12:20 +0000

    implement simple search along with a random generator

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <chrono>
 #include <functional>
 #include "fibonacci.hpp"
+#include <vector>
 
 template <typename Func, typename... Args>
 auto measure_runtime(Func &&func, Args &&...args)
@@ -18,9 +19,48 @@ auto measure_runtime(Func &&func, Args &&...args)
     std::cout << "Time passed: " << passed << " ns" << std::endl;
 }
 
+std::vector<std::int8_t> random_generator() 
+{
+    std::srand(std::time(nullptr)); // seed with current time
+
+    std::vector<std::int8_t> array;
+    array.reserve(10);
+
+    for (int i = 0; i < 10; ++i) {
+        array.push_back(std::rand() % 100); // random number between 0 and 99
+    }
+
+    return array;
+}
+
+std::int8_t search_min(std::vector<std::int8_t> array) 
+{
+    std::int8_t min = array[0];
+    
+    for(std::int8_t i = 0; i < array.size(); ++i) {
+        if(array[i] < min)
+        {
+            min = array[i];
+        }
+    }
+    
+    std::cout << "Given array:  ";
+    
+    for (int n : array) {
+        std::cout << n << " ";
+    }
+
+    std::cout << "selected minimum number is " << static_cast<int>(min) << std::endl;
+
+    return min;
+}
+
 int main()
 {
+    std::vector<std::int8_t> array = random_generator();
 
     measure_runtime(calcualte_fibonacci_with_loop);
     measure_runtime(calcualte_fibonacci_with_recursion, 0, 1, 17);
+    measure_runtime(search_min, array);
+
 }
