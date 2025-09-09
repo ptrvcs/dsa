f7d6b864041446abdedaa92f5b655294c70a6145
ptrvcs <petrovicsi.90@gmail.com>
2025-09-09 17:18:15 +0000

    Organize fibonacci in a separate file

--- main.cpp
+++ main.cpp
@@ -1,32 +1,7 @@
 #include <iostream>
 #include <chrono>
 #include <functional>
-
-void calcualte_fibonacci_with_loop()
-{
-    int last_before_number = 0;
-    int last_number = 1;
-
-    for (int i = 0; i < 17; i++)
-    {
-        int result = last_before_number + last_number;
-        std::cout << result << std::endl;
-        last_before_number = last_number;
-        last_number = result;
-    }
-}
-
-void calcualte_fibonacci_with_recursion(int last_before_number, int last_number, int remaining)
-{
-    if (remaining == 0)
-        return;
-
-    int result = last_before_number + last_number;
-    std::cout << result << std::endl;
-    last_before_number = last_number;
-    last_number = result;
-    calcualte_fibonacci_with_recursion(last_before_number, last_number, remaining - 1);
-}
+#include "fibonacci.hpp"
 
 template <typename Func, typename... Args>
 auto measure_runtime(Func &&func, Args &&...args)
